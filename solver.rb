055b5d85b78895f6b16cea398bcdb7fe8d9d0458
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-02-04 20:51:59 +0500

    Add reverse and fizzbuzz methods

--- solver.rb
+++ solver.rb
@@ -1,13 +1,28 @@
 # frozen_string_literal: true
 
 # class Solver
-
 class Solver
   def factorial(int)
     if int.negative?
       'invalid input'
     else
       int.zero? || int == 1 ? 1 : int * factorial(int - 1)
+    end
+  end
+
+  def reverse(str)
+    str.reverse
+  end
+
+  def fizzbuzz(int)
+    if (int % 3).zero? && (int % 5).zero?
+      'fizzbuzz'
+    elsif (int % 5).zero?
+      'buzz'
+    elsif (int % 3).zero?
+      'fizz'
+    else
+      int.to_s
     end
   end
 end

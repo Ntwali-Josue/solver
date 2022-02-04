bf10753e6c023a99b2b033ca570b5f9957a62cd5
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-02-04 20:23:25 +0500

    Add method factorial to Solver calss

--- solver.rb
+++ solver.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
+# class Solver
+
 class Solver
-  
+  def factorial(int)
+    if int.negative?
+      'invalid input'
+    else
+      int.zero? || int == 1 ? 1 : int * factorial(int - 1)
+    end
+  end
 end
